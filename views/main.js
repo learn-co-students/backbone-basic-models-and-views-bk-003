189aae6c760db36317e34ba71da7e4d227f4a579
ZalmanB <zalmanblau@gmail.com>
2015-11-19 18:48:48 -0500

    are you, or are you not?

--- views/main.js
+++ views/main.js
@@ -5,25 +5,22 @@ FormView = Backbone.View.extend({
   },
   initialize : function() {
     // call on this instance's render function here
+    this.render()
   },
-  render : function() {
-    // here is the html you want to append to $el:
-    // <form>
-    //  <label for="name"></label>
-    //  <input id="name" type="text" name="name">
-    //  <input id= "mySubmit" type="submit" value="submit">
-    // </form>
-    
-    // remember to return the instance of the FormView
+  render : function() { 
+    this.$el.append("<form><label for='name'></label><input id='name' type='text' name='name'><input id= 'mySubmit' type='submit' value='submit'></form>")
   },
-  submitHandler : function() {
+  events : {
+    'click #mySubmit' : 'submitHandler',
+  },
+  submitHandler : function(event) {
     // make sure this function accepts the event parameter
-    
     // prevent the default behavior (submission) of the form 
     // (call preventDefault on the event) 
-    
+    event.preventDefault()
     // save the text is in the input field with an id of name here
-
+    name = $("#name").val()
     // set the model's name to have a value of the name variable here
+    this.model.set({name: name})
   }
 })
