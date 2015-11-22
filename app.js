d6d97f158c7e4017bb9d0fb731d745fd6f5076f6
Damian Lajara <damiansito93@gmail.com>
2015-11-22 13:23:37 -0500

    models and views

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ app = {};
 
 function bootstrap() {
   // instantiate a new user object and save it as a variable here
-
+  var user = new User();
+  var formView = new FormView({model: user});
+  // var formView = new FormView
+  app.formView = formView
+  $("body").append(formView.el)
   // instantiate your view object here:
   //   * it should take one argument, an object:
   //      - the parameter object should have a property called "model"
-  //        which should point to the new instance of the user  
+  //        which should point to the new instance of the user
 
   // attach your view object to the app object as a property called "formView"
 
   // append your view object's HTML to the DOM here
 }
-
-bootstrap();
--- views/main.js
+++ views/main.js
@@ -2,28 +2,33 @@ FormView = Backbone.View.extend({
   events : {
     // you'll want to add a submit event to the form
     // it should trigger the submitHandler function
+    "submit" : "submitHandler"
   },
   initialize : function() {
     // call on this instance's render function here
+    this.render();
   },
   render : function() {
     // here is the html you want to append to $el:
-    // <form>
-    //  <label for="name"></label>
-    //  <input id="name" type="text" name="name">
-    //  <input id= "mySubmit" type="submit" value="submit">
-    // </form>
-    
+  var html = '<form>'+
+     '<label for="name"></label>'+
+     '<input id="name" type="text" name="name">'+
+     '<input id="mySubmit" type="submit" value="submit">'+
+     '</form>';
+
+    this.$el.append(html);
+    return this;
     // remember to return the instance of the FormView
   },
-  submitHandler : function() {
+  submitHandler : function(event) {
     // make sure this function accepts the event parameter
-    
-    // prevent the default behavior (submission) of the form 
-    // (call preventDefault on the event) 
-    
+    // prevent the default behavior (submission) of the form
+    // (call preventDefault on the event)
+    event.preventDefault();
     // save the text is in the input field with an id of name here
-
+    var text = $("#name").val();
+    console.log(text);
     // set the model's name to have a value of the name variable here
+    this.model.set({name: text});
   }
 })
