80f2bf6bf518486fb5d0b5ee063aaa0fdd40747b
djoseph1250 <djoseph1250@gmail.com>
2015-11-26 15:31:04 -0500

    solved

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ app = {};
 
 function bootstrap() {
   // instantiate a new user object and save it as a variable here
-
+  var user = new User();
   // instantiate your view object here:
   //   * it should take one argument, an object:
   //      - the parameter object should have a property called "model"
   //        which should point to the new instance of the user  
+  app.formView = new FormView({model : user});	
 
   // attach your view object to the app object as a property called "formView"
 
   // append your view object's HTML to the DOM here
+  $('body').append(app.formView.el);
+  // return app.formView;
+  // return this;
 }
 
 bootstrap();
